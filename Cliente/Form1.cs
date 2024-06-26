05bed0b00040443e425434caaad564ddd4cc9e1e
DaniNeuer <daniel.tapia01@epn.edu.ec>
2024-06-25 19:00:14 -0500

    Add files via upload

---
+++ Cliente/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using Protocolo;
+
+namespace Cliente
+{
+    public partial class FrmValidador : Form
+    {
+        // Ahora, modificamos el cliente para que use la clase Protocolo
+        private TcpClient remoto;
+        private NetworkStream flujo;
+
+        public FrmValidador()
+        {
+            InitializeComponent();
+        }
+        // Evento que se dispara cuando se carga el formulario
+        private void FrmValidador_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Intento de conexión al servidor en localhost en el puerto 8080
+                remoto = new TcpClient("127.0.0.1", 8080);
+                flujo = remoto.GetStream();
+            }
+            catch (SocketException ex)
+            {
+                // Mostrar un mensaje de error si no se puede establecer la conexió
+                MessageBox.Show("No se puedo establecer conexión " + ex.Message,
+                    "ERROR");
+            }
+            /*
+            finally 
+            {
+                flujo?.Close();
+                remoto?.Close();
+            }
+            */
+            // Deshabilitar paneles y checkboxes al inicio.
+            panPlaca.Enabled = false;
+            chkLunes.Enabled = false;
+            chkMartes.Enabled = false;
+            chkMiercoles.Enabled = false;
+            chkJueves.Enabled = false;
+            chkViernes.Enabled = false;
+            chkDomingo.Enabled = false;
+            chkSabado.Enabled = false;
+        }
+        // Evento que se dispara cuando se hace clic en el botón Iniciar
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contraseña = txtPassword.Text;
+            if (usuario == "" || contraseña == "")
+            {
+                // Mostrar advertencia si no se ingresan usuario o contraseña
+                MessageBox.Show("Se requiere el ingreso de usuario y contraseña",
+                    "ADVERTENCIA");
+                return;
+            }
+            // Crear un pedido de ingreso con los datos de usuario y contraseña
+            Pedido pedido = new Pedido
+            {
+                Comando = "INGRESO",
+                Parametros = new[] { usuario, contraseña }
+            };
+            // Ejecutar la operación de ingreso
+            //Respuesta respuesta = HazOperacion(pedido);
+            //modificacion
+            Respuesta respuesta = Protocolo.Protocolo.HazOperacion(pedido, flujo);
+            if (respuesta == null)
+            {
+                // Mostrar error si la respuesta es nula
+                MessageBox.Show("Hubo un error", "ERROR");
+                return;
+            }
+            // Verificar la respuesta y habilitar/deshabilitar paneles según el resultado
+            if (respuesta.Estado == "OK" && respuesta.Mensaje == "ACCESO_CONCEDIDO")
+            {
+                panPlaca.Enabled = true;
+                panLogin.Enabled = false;
+                MessageBox.Show("Acceso concedido", "INFORMACIÓN");
+                txtModelo.Focus();
+            }
+            else if (respuesta.Estado == "NOK" && respuesta.Mensaje == "ACCESO_NEGADO")
+            {
+                panPlaca.Enabled = false;
+                panLogin.Enabled = true;
+                MessageBox.Show("No se pudo ingresar, revise credenciales",
+                    "ERROR");
+                txtUsuario.Focus();
+            }
+        }
+        
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            string modelo = txtModelo.Text;
+            string marca = txtMarca.Text;
+            string placa = txtPlaca.Text;
+            //Se crea el pedido con los el valor de comadno CALCULO y los parametros los ingresados 
+            Pedido pedido = new Pedido
+            {
+                Comando = "CALCULO",
+                Parametros = new[] { modelo, marca, placa }
+            };
+            // la respuesta tomara los devuelto con el metodo HazOperacion con el pedido 
+            //Respuesta respuesta = HazOperacion(pedido);
+            //Modificacion
+            Respuesta respuesta = Protocolo.Protocolo.HazOperacion(pedido, flujo);
+            if (respuesta == null)
+            {
+                MessageBox.Show("Hubo un error", "ERROR");
+                return;
+            }
+            // Procesar la respuesta
+            if (respuesta.Estado == "NOK")
+            {
+                //Si la respues por parte del servidor es NOK se envia un messageBox
+                MessageBox.Show("Error en la solicitud.", "ERROR");
+                chkLunes.Checked = false;
+                chkMartes.Checked = false;
+                chkMiercoles.Checked = false;
+                chkJueves.Checked = false;
+                chkViernes.Checked = false;
+            }
+            else
+            {
+                // si la respuesta es OK se procesa la respuesta 
+                var partes = respuesta.Mensaje.Split(' ');
+                MessageBox.Show("Se recibió: " + respuesta.Mensaje,
+                    "INFORMACIÓN");
+                byte resultado = Byte.Parse(partes[1]);
+                switch (resultado)
+                {
+                    case 0b00100000:
+                        chkLunes.Checked = true;
+                        chkMartes.Checked = false;
+                        chkMiercoles.Checked = false;
+                        chkJueves.Checked = false;
+                        chkViernes.Checked = false;
+                        break;
+                    case 0b00010000:
+                        chkMartes.Checked = true;
+                        chkLunes.Checked = false;
+                        chkMiercoles.Checked = false;
+                        chkJueves.Checked = false;
+                        chkViernes.Checked = false;
+                        break;
+                    case 0b00001000:
+                        chkMiercoles.Checked = true;
+                        chkLunes.Checked = false;
+                        chkMartes.Checked = false;
+                        chkJueves.Checked = false;
+                        chkViernes.Checked = false;
+                        break;
+                    case 0b00000100:
+                        chkJueves.Checked = true;
+                        chkLunes.Checked = false;
+                        chkMartes.Checked = false;
+                        chkMiercoles.Checked = false;
+                        chkViernes.Checked = false;
+                        break;
+                    case 0b00000010:
+                        chkViernes.Checked = true;
+                        chkLunes.Checked = false;
+                        chkMartes.Checked = false;
+                        chkMiercoles.Checked = false;
+                        chkJueves.Checked = false;
+                        break;
+                    default:
+                        chkLunes.Checked = false;
+                        chkMartes.Checked = false;
+                        chkMiercoles.Checked = false;
+                        chkJueves.Checked = false;
+                        chkViernes.Checked = false;
+                        break;
+                }
+            }
+        }
+
+        private void btnNumConsultas_Click(object sender, EventArgs e)
+        {
+            String mensaje = "hola";
+            
+            Pedido pedido = new Pedido
+            {
+                Comando = "CONTADOR",
+                Parametros = new[] { mensaje }
+            };
+
+            //Respuesta respuesta = HazOperacion(pedido);
+            //modificacion
+            Respuesta respuesta = Protocolo.Protocolo.HazOperacion(pedido, flujo);
+            if (respuesta == null)
+            {
+                MessageBox.Show("Hubo un error", "ERROR");
+                return;
+            }
+
+            if (respuesta.Estado == "NOK")
+            {
+                MessageBox.Show("Error en la solicitud.", "ERROR");
+
+            }
+            else
+            {
+                var partes = respuesta.Mensaje.Split(' ');
+                MessageBox.Show("El número de pedidos recibidos en este cliente es " + partes[0],
+                    "INFORMACIÓN");
+            }
+        }
+
+        private void FrmValidador_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (flujo != null)
+                flujo.Close();
+            if (remoto != null)
+                if (remoto.Connected)
+                    remoto.Close();
+        }
+    }
+}
---
+++ Servidor/Servidor.cs
@@ -0,0 +1,119 @@
+﻿// *********************************************
+// **************Encabezado*********************
+// Nombre: Daniel Tapia
+// Fecha de entrega: 26/06/2024
+
+// *********************************************
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using Protocolo;
+
+namespace Servidor
+{
+    class Servidor
+    {
+        // Se declara el objeto TcpListener con el nombre escuchador
+        private static TcpListener escuchador;
+        // Se declara un diccionario en la cual va a tener como elementos un string relacionado con un int 
+        private static Dictionary<string, int> listadoClientes
+            = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            // Creamos un try y catch por si hay falla de conexion al cliente
+            try
+            {
+                // Iniciamos con el escucha por parte del servidor en cualquier direccion IP en el puerto 8080
+                escuchador = new TcpListener(IPAddress.Any, 8080);
+                // Se comienza el Listener
+                escuchador.Start();
+                // Se muestra en consola que el servidor inicio 
+                Console.WriteLine("Servidor inició en el puerto 5000...");
+
+                while (true)
+                {
+                    // En un bucle infinito en la cual se crea el cliente TCP y sera aceptado por el escuchador
+                    TcpClient cliente = escuchador.AcceptTcpClient();
+                    // Se imprime el cliente conectado en elpuerto  asignado
+                    Console.WriteLine("Cliente conectado, puerto: {0}", cliente.Client.RemoteEndPoint.ToString());
+                    // Se crea un hilo para el manejo del cliente en la cual se acepto la conexion  con el metodo ManipuladorCliente
+                    Thread hiloCliente = new Thread(ManipuladorCliente);
+                    // Se inicia el hilo 
+                    hiloCliente.Start(cliente);
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Error de socket al iniciar el servidor: " +
+                    ex.Message);
+            }
+            finally 
+            {
+                // Si escuchador es difernte de null se para con el stop()
+                escuchador?.Stop();
+            }
+        }
+        
+        // se crea un mentodo en la cual se manejara los mensajes enviados por el cliente con un parametro de entrada de tipo object
+        private static void ManipuladorCliente(object obj)
+        {
+            // Se realiza un cast al objeto que se ingresa a clienteTCP
+            TcpClient cliente = (TcpClient)obj;
+            // Se crea el flujo de red
+            NetworkStream flujo = null;
+            try
+            {
+                // indicamos que el flujo creado sera el flujo del cliente
+                flujo = cliente.GetStream();
+                // Se crea los dos buffers de transimision y de recepcion
+                byte[] bufferTx;
+                byte[] bufferRx = new byte[1024];
+                int bytesRx;
+                // Verificamos si hay datos en el flujo de red
+                while ((bytesRx = flujo.Read(bufferRx, 0, bufferRx.Length)) > 0)
+                {
+                    // obtnenemos los datos enviados del cliente y lo decodificamos en UTF8
+                    string mensajeRx =
+                        Encoding.UTF8.GetString(bufferRx, 0, bytesRx);
+                    // realizamos el metodo Procesar de la clase Pedido del proyecto protocolo
+                    Pedido pedido = Pedido.Procesar(mensajeRx);
+                    // Se imprime en consola el mensaje procesado 
+                    Console.WriteLine("Se recibio: " + pedido);
+                    // Se crea un string en lacual se guardara la direccion del cliente
+                    string direccionCliente =
+                        cliente.Client.RemoteEndPoint.ToString();
+                    // Al tener la direccion del cliente usamos el metodo ResolverPedido para enviar las respuesta 
+                    // usando como parametros lo pedido y la direccion del cleinte
+
+                    //Respuesta respuesta = ResolverPedido(pedido, direccionCliente);
+                    //modificacion
+
+                    Respuesta respuesta = Protocolo.Protocolo.ResolverPedido(pedido, direccionCliente, listadoClientes);
+                    Console.WriteLine("Se envió: " + respuesta);
+                    // Ya teniendo la respuesta se envia al buffer de trasmision lo codificamos usando UTF8
+                    bufferTx = Encoding.UTF8.GetBytes(respuesta.ToString());
+                    //se envia usando el metodo Write
+                    flujo.Write(bufferTx, 0, bufferTx.Length);
+                }
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Error de socket al manejar el cliente: " + ex.Message);
+            }
+            finally
+            {
+                // Al finalizar se cierra el flujo y al cliente
+                flujo?.Close();
+                cliente?.Close();
+            }
+        }
+     
+    }
+}
